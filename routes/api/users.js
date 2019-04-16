403115df2330ca02809413f3d5a3542da105db62
Dayan Petrov <dayan.petroff@gmail.com>
2019-04-16 12:11:12 +0300

    Login logic /api/users/login

--- routes/api/users.js
+++ routes/api/users.js
@@ -56,5 +56,34 @@ router.post('/register', (req, res) => {
         }) 
 });
 
+/*  
+@route   POST api/users/register
+@desc    Login user / Return JWT 
+@access  Public 
+*/
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //find the user by email
+    User.findOne({ email })
+        .then(user => {
+            //check for user
+            if(!user) {
+                return res.status(404).json({email: 'User not found!'});
+            }
+
+            //check password
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch) {
+                        //generate token
+                        res.json({ msg: 'Success!'})
+                    } else {
+                        return res.status(400).json({password: 'Password incorrect!'});
+                    }
+                });
+        });
+});
 
 module.exports = router;
