964ab4d0e15a53b35de690562523e37e9729fac4
Dayan Petrov <dayan.petroff@gmail.com>
2019-04-16 11:13:54 +0300

    Initial routes setup: posts, users, profile

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+//routes
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
 
 //DB config
@@ -15,6 +20,11 @@ mongoose
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+//use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const PORT = process.env.PORT || 5000;
 
