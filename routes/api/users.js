c62539906ee5f1a9df45fa2094720f15b9b59ba2
Dayan Petrov <dayan.petroff@gmail.com>
2019-04-16 12:01:17 +0300

    Register user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 //authentication file
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+//load model
+const User = require('../../models/User');
 
 /*  
 @route   GET api/users/test
@@ -8,5 +13,48 @@ const router = express.Router();
 @access  Public 
 */
 router.get('/test', (req, res) => res.json({msg: "Users route works!"}));
+
+/*  
+@route   POST api/users/register
+@desc    Register a user
+@access  Public 
+*/
+router.post('/register', (req, res) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if(user) {
+                return res.status(400).json({ email: "Email already exists! "});
+            } else {
+                //get a gravatar associated to the email or return a default image
+                const avatar = gravatar.url(req.body.email, {
+                    s: '200', //size
+                    r: 'pg', //rating
+                    d: 'mm' //default image
+                });
+
+                //create the user object
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar: avatar,
+                    password: req.body.password
+                });
+
+                //hash the password and save the user to the db
+                bcrypt.genSalt(10, (salt_error, salt) => {
+                    if(salt_error) throw salt_error;
+                    bcrypt.hash(newUser.password, salt, (hash_error, hash) => {
+                        if(hash_error) throw hash_error;
+                        newUser.password = hash;
+                        newUser
+                            .save()
+                            .then(user => res.json(user))
+                            .catch(save_error => console.log(save_error));
+                    })
+                });
+            }
+        }) 
+});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 //routes
 const users = require('./routes/api/users');
@@ -8,12 +9,16 @@ const posts = require('./routes/api/posts');
 
 const app = express();
 
+//body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 //DB config
 const db = require('./config/keys').mongoURI;
 
 //connect to MongoDB
 mongoose
-    .connect(db)
+    .connect(db, { useNewUrlParser: true })
     .then(() => console.log('MongoDB connected'))
     .catch((error) => console.log(error));
 
