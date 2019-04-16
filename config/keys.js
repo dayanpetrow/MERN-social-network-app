eeaf8acdfb17a0dfd671f412f41e9af97ea7ec3a
Dayan Petrov <dayan.petroff@gmail.com>
2019-04-16 10:59:21 +0300

    MongoDB connection to mLab with mongoose

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+//mlab db credentials
+const MONGODB_USER = 'dayan';
+const MONGODB_PASSWORD = 'udemymern123';
+
+module.exports = {
+    mongoURI: `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@ds129536.mlab.com:29536/udemy-mern-social-network`
+}
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+//connect to MongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB connected'))
+    .catch((error) => console.log(error));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
