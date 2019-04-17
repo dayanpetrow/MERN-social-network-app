2c83e458256cab516e15974fe245d593cc13dbdb
Dayan Petrov <dayan.petroff@gmail.com>
2019-04-17 15:46:26 +0300

    Like and unlike posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -86,4 +86,53 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
     newPost.save().then(post => res.json(post));
 });
 
+/*  
+@route   POST api/posts/like/:id
+@desc    Like post
+@access  Private
+*/
+router.post('/like/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOne({ user: req.user.id })
+    .then(profile => {
+        Post.findById(req.params.id)
+            .then(post => {
+                if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+                    return res.status(400).json({ alreadyLiked: "Post is already liked!" });
+                }
+
+                //add user_id to like array
+                post.likes.unshift({ user: req.user.id });
+
+                post.save().then(post => res.json(post));
+            })
+            .catch(err => res.status(404).json(err));
+    }).catch(err => res.status(404).json(err));
+});
+
+/*  
+@route   POST api/posts/unlike/:id
+@desc    Unlike post
+@access  Private
+*/
+router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOne({ user: req.user.id })
+    .then(profile => {
+        Post.findById(req.params.id)
+            .then(post => {
+                if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+                    return res.status(400).json({ alreadyLiked: "Post is not yet liked!" });
+                }
+
+                //get remove index
+                const removeIndex = post.likes.map(item => item.user.toString)
+                    .indexOf(req.user.id);
+
+                post.likes.splice(removeIndex, 1);
+
+                post.save().then(post => res.json(post));
+            })
+            .catch(err => res.status(404).json(err));
+    }).catch(err => res.status(404).json(err));
+});
+
 module.exports = router;
